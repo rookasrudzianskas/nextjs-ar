0db4c9b6fc9e06e1177a95b0a5bd192259dc4264
Rokas <rookas.rudzenskas@gmail.com>
2023-01-26 11:35:28 +0200

    AR intergration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,58 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import React, { Suspense, useState } from 'react'
+import { Interactive, XR, ARButton, Controllers } from '@react-three/xr'
+import { Text } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+
+function Box({ color, size, scale, children, ...rest }: any) {
+  return (
+    <mesh scale={scale} {...rest}>
+      <boxBufferGeometry args={size} />
+      <meshPhongMaterial color={color} />
+      {children}
+    </mesh>
+  )
+}
+
+function Button(props: any) {
+  const [hover, setHover] = useState(false)
+  const [color, setColor] = useState<any>('blue')
+
+  const onSelect = () => {
+    setColor((Math.random() * 0xffffff) | 0)
+  }
+
+  return (
+    <Interactive onHover={() => setHover(true)} onBlur={() => setHover(false)} onSelect={onSelect}>
+      <Box color={color} scale={hover ? [0.6, 0.6, 0.6] : [0.5, 0.5, 0.5]} size={[0.4, 0.1, 0.1]} {...props}>
+        <Suspense fallback={null}>
+          <Text position={[0, 0, 0.06]} fontSize={0.05} color="#000" anchorX="center" anchorY="middle">
+            Hello react-xr!
+          </Text>
+        </Suspense>
+      </Box>
+    </Interactive>
+  )
+}
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="w-full h-screen">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      Hello world!
+      <ARButton />
 
+      <Canvas>
+        <XR referenceSpace="local">
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} />
+          <Button position={[0, 0.1, -0.2]} />
+          <Controllers />
+        </XR>
+      </Canvas>
     </div>
   )
 }

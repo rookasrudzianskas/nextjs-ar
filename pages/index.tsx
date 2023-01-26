15b2f3861e608c730f43482bb25e6f700fbda405
Rokas <rookas.rudzenskas@gmail.com>
2023-01-26 11:22:41 +0200

    Add a new feature
    This is a new feature that does something.
    
    It is a good feature.

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      Hello world!
+
+    </div>
+  )
+}
+
+export default Home
